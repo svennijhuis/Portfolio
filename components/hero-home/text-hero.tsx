bd9bef0fa62d9207e32415614584c94a978f7948
Sven Nijhuis <sanijhuis@live.nl>
2022-12-15 22:34:37 +0100

    font

--- components/hero-home/text-hero.tsx
+++ components/hero-home/text-hero.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@ui/button";
 import styles from "./hero-home.module.scss";
 
+import { Inter } from "@next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
 export default function TextHero() {
   return (
     <article className="md:sticky md:top-0 md:h-[100vh] flex flex-col justify-center md:col-span-1  h-auto">
-      <h1 className={`text-40 leading-45 md:text-60 md:leading-70 font-medium text-white ${styles.h1}`}>
+      <h1 className={`text-50 leading-60 md:text-60 md:leading-70 font-bold text-white ${styles.h1} ${inter.className}`}>
         Hi. i’m <span>Sven</span> Webdeveloper.
       </h1>
       <h2 className="mb-3 text-16 leading-26 md:text-20 md:leading-30 text-white">
