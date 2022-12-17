6394db49cf20e8d73187c5af30ad046e02dedd3c
Sven Nijhuis <sanijhuis@live.nl>
2022-12-17 18:57:47 +0100

    background text gradient

--- components/text/call-to-action.tsx
+++ components/text/call-to-action.tsx
@@ -10,7 +10,7 @@ export default function CallToAction({
   return (
     <section className="py-10 md:py-12 bg-black">
       <div className="container flex flex-col gap-3">
-        <h3 className="text-40 leading-50 md:text-60 md:leading-70 font-normal text-white md:w-[60%]">
+        <h3 className="text-40 leading-50 md:text-60 md:leading-70 font-bold text-white md:w-[60%] gradient-text">
           {text}
         </h3>
         <Button intent="primary">{button}</Button>
