1b0db65e7470389ba2ed93f674562d65c27d2434
Sven Nijhuis <sanijhuis@live.nl>
2022-12-28 00:03:09 +0100

    hover cases wip

--- components/cards/cases.tsx
+++ components/cards/cases.tsx
@@ -6,7 +6,7 @@ import useIntersectionObserver from "../../hooks/use-intersection-observer";
 
 export default function CardCases() {
   return (
-    <article className={`relative grid grid-cols-3 gap-3  md:gap-6 py-6 md:py-0 md:my-6 border-[#a6a6a6] border-b-[0.5px] md:border-none ${styles.img_hover}`}>
+    <article className={`relative grid grid-cols-3 gap-3  md:gap-6 py-6 md:py-0 md:my-6 border-[#a6a6a6] border-b-[0.5px] md:border-none ${styles.block_hover}`}>
       <Image
         className="col-span-3 sm:col-span-1 aspect-video md:aspect-square w-full h-auto object-cover"
         src=""
@@ -16,7 +16,7 @@ export default function CardCases() {
       />
       <div className="col-span-3 sm:col-span-2 flex flex-col justify-start md:border-[#a6a6a6] md:border-b-[0.5px]">
         {" "}
-        <h3 className="text-18 leading-28 md:text-22 md:leading-27 font-normal text-black mb-3">
+        <h3 className="text-18 leading-28 md:text-22 md:leading-27 font-normal text-black mb-1 md:mb-3">
           sdaf
         </h3>
         <h3 className="text-25 leading-35 md:text-35 md:leading-40 font-normal text-black md:w-[90%] md:mb-3">
--- components/hero-home/slider-mobile.tsx
+++ components/hero-home/slider-mobile.tsx
@@ -10,16 +10,13 @@ export default function SliderMobile() {
   return (
     <SectionParallax>
       <article className=" mx-[-20px] overflow-hidden">
-        <Parallax translateX={[-200, 20]} easing="easeOutQuad">
+        <Parallax translateX={[-200, -20]} easing="easeOutQuad">
           <div className={styles.tag__list}>
             <InfiniteLoopSlider
               duration={{
                 "--duration": `${"0"}ms`,
               }}
             >
-              {/* {numbersCopy.map((tag, index) => (
-          <Tag text={tag} key={index} />
-        ))} */}
               {Array.from({ length: 10 }).map((_, index) => (
                 <CardHero key={index + 1} />
               ))}
